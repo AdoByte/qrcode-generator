f33a3c57fe18d9ce52380af7a72c81053ad7015b
Adobyte <adobyte77@gmail.com>
2023-12-10 21:24:15 +0100

    Create method that generates QR code
    - Method logic is placed within a try block incase the user input
      exceeds QR allowed input
    - . . .any other exceptions

--- qrcode_generator.py
+++ qrcode_generator.py
@@ -5,3 +5,18 @@ class MyQrCode:
 
     def __init__(self, q_size, q_padding):
         self.qr = qrcode.QRCode(box_size=q_size, border=q_padding)
+
+    def create_qrcode(self, file_name, fg_color, bg_color):
+
+        user_input = "https://www.google.com"
+
+        try:
+            self.qr.add_data(user_input)
+            qr_image = self.qr.make_image(fill_color=fg_color, back_color=bg_color)
+            qr_image.save("test_qr.png")
+
+        except Exception as e:
+            print(f"Error: {e}")
+
+
+
